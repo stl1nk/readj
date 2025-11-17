61bebfe2d24fd7633b12ccfb7a3373fc0968c066
st1nk <stlink2009@gmail.com>
2025-11-17 05:39:28 +0100

    test

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,5 +1,56 @@
-function Home() {
-    return <div>Home</div>
-    }
+import {useState, useEffect} from "react"
+import api from "../api"
 
+function Home() {
+    const [notes, setNotes] = useState([]);
+    const [content, setContent] = useState("");
+    const [title, setTitle] = useState("");
+
+    useEffect(() => {getNotes();}, [])
+
+    const getNotes = () => {
+        api.get("/api/notes/").then((res) => res.data).then((data) => {setNotes(data); console.log(data)}).catch((err) => alert(err));
+        }
+
+
+    const deleteNote = (id) => {
+        api.delete(`/api/notes/delete/${id}/`).then((res) => {
+            if (res.status === 204) alert("Note deleted");
+            else alert("failed delete");
+            getNotes();
+            }).catch((err) => alert(err));
+
+        }
+
+    const createNote = (e) => {
+        e.preventDefault()
+        api.post("/api/notes/", {content, title}).then((res) =>{
+            if (res.status === 201 ) alert("created");
+            else alert("Failed make");
+            getNotes();
+            }).catch((err) => {
+                console.error("Error:", err.response);
+                alert(err)} );
+
+        }
+
+
+    return (<div>
+        <div>
+            <h2>Notes</h2>
+        </div>
+        <h2>Create a note</h2>
+        <form onSubmit={createNote}>
+            <label htmlFor="title">Title:</label>
+            <br/>
+            <input type="text" id="title" name="title" required onChange={(e) => setTitle(e.target.value)} value={title}/>
+            <label htmlFor="content">Content:</label>
+            <br/>
+            <textarea id="content" name="content" required value={content} onChange={(e) => setContent(e.target.value)}></textarea>
+            <br/>
+            <input type="submit" value="Submit"></input>
+        </form>
+       </div>
+       );
+   }
 export default Home
